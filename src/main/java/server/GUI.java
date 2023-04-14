0ad25dbb0cc156f02e4f645a43b6b90107634c2f
kerwan0916 <jkevin.fuss@gmail.com>
2023-04-14 13:53:07 -0400

    User Interface basics complete, only thing missing are buttons to load info

--- src/main/java/server/GUI.java
+++ src/main/java/server/GUI.java
@@ -1,6 +1,9 @@
 package server;
 
 import javafx.geometry.Orientation;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
@@ -9,12 +12,13 @@ import javafx.scene.layout.VBox;
 import javafx.scene.layout.HBox;
 import javafx.scene.control.SplitPane;
 import javafx.application.Application;
+import javafx.scene.control.TextField;
 
 public class GUI {
 
     // TODO Manque du code ici pour le extends du server pour le class
 
-    @Override
+    // @Override
     public void start (Stage primaryStage) {
         // Création des deux scènes avec leur contenu respectif
         VBox ListeDesCours = new VBox(new Label("Liste des cours"));
@@ -29,6 +33,36 @@ public class GUI {
         FormulaireDInscription.setPrefWidth(200);
 
         Scene scene = new Scene(splitPane, 400, 300);
+
+
+
+        // Création du tableau qui affiche les cours disponibles
+        TableView<String> table = new TableView<>();
+        ListeDesCours.getChildren().add(table);
+
+        TableColumn<String, String> code = new TableColumn<>("Code");
+        TableColumn<String, String> cours = new TableColumn<>("Cours");
+
+        table.getColumns().addAll(code, cours);
+        // TODO pour ajouter des items dans le table on fait table.getItems().addAll( "row 1 data 1, row 1 data 2, etc.);
+        // TODO utiliser ca avec le bouton charger pour load le information des cours dans le tableau
+        // TODO dropdown list pour choisir la session avec options: Hiver, Automne, Ete
+
+        // Création de la grille pour le formulaire d'inscription
+        GridPane formulaire = new GridPane();
+        FormulaireDInscription.getChildren().add(formulaire);
+
+        formulaire.add(new Label("Prénom"), 0, 0);
+        formulaire.add(new TextField(), 1, 0);
+        formulaire.add(new Label("Nom"), 0, 1);
+        formulaire.add(new TextField(), 1, 1);
+        formulaire.add(new Label("Email"), 0, 2);
+        formulaire.add(new TextField(),1, 2);
+        formulaire.add(new Label("Matricule"), 0, 3);
+        formulaire.add(new TextField(), 1, 3);
+
+        // TODO bouton envoyer
+
 
         primaryStage.setScene(scene);
         primaryStage.setTitle("Inscription UdeM");
