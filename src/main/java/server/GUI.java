18c000f21a7c25dd44e281a132d07c80a7069728
kerwan0916 <jkevin.fuss@gmail.com>
2023-04-16 01:05:08 -0400

    Ajout du dropdown button and tried the test file, javafx still not letting me run the application..

--- src/main/java/server/GUI.java
+++ src/main/java/server/GUI.java
@@ -1,23 +1,21 @@
 package server;
 
+import javafx.collections.FXCollections;
 import javafx.geometry.Orientation;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.layout.HBox;
-import javafx.scene.control.SplitPane;
 import javafx.application.Application;
-import javafx.scene.control.TextField;
 
-public class GUI {
- // TODO Manque du code ici pour le extends du server pour le class
 
-    //@Override
+public class GUI extends Application{
+        public static void main(String[] args) {
+            launch(args);
+        }
+    @Override
     public void start (Stage primaryStage) {
         // Création des deux scènes avec leur contenu respectif
         VBox ListeDesCours = new VBox(new Label("Liste des cours"));
@@ -45,11 +43,21 @@ public class GUI {
         table.getColumns().addAll(code, cours);
         // TODO pour ajouter des items dans le table on fait table.getItems().addAll( "row 1 data 1, row 1 data 2, etc.);
         // TODO utiliser ca avec le bouton charger pour load le information des cours dans le tableau
-        // TODO dropdown list pour choisir la session avec options: Hiver, Automne, Ete
 
+
+        // Création du dropdown list button pour le choix de la session
+        ComboBox<String> ChoixSession = new ComboBox<>();
+        ChoixSession.setItems(FXCollections.observableArrayList(
+                // options de choix ex. Hiver, Automne, Été, c'est une série de string séparés par des virgules
+                ));
+
+        // HBox vide pour laisser un espace entre le titre et le formulaire
+        HBox espace = new HBox();
+        FormulaireDInscription.getChildren().add(espace);
         // Création de la grille pour le formulaire d'inscription
         GridPane formulaire = new GridPane();
         FormulaireDInscription.getChildren().add(formulaire);
+
 
         formulaire.add(new Label("Prénom"), 0, 0);
         formulaire.add(new TextField(), 1, 0);
